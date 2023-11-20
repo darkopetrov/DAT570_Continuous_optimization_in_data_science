6c0079ba933f9f714b11f4fe0599eafe557c9f5d
Darko Petrov <darko.petrov1099@gmail.com>
2023-11-20 17:04:03 +0100

    Module 3 files

---
+++ Module_3/LS_constrained.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def L1projection(x,r):
+    #x is the point and r is the radius
+    # Implement the dual gradient ascend algorithm with a suitable step size
+
+def L2projection(x,r):
+    #x is the point and r is the radius
+    # Implement by the closed form expression
+
+def LINFprojection(x,r):
+    #x is the point and r is the radius
+    # Implement by the closed form expression
+
+
+# Check your functions by projecting some random points and checking the result.
+
+
+# Implemnt PGD
+# step size: mu=...
+# number of iterations: T
+# initialize theta ,mu
+
+for i in range(T):
+    # calculate the gradients and apply GD
+    # Apply projection
+    # calculate the objective value and store it
+    
+
